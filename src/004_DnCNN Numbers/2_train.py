3b9be8284f79cfd599a5509ea4d012f8ca910a9e
Atamarado <ginescarreto@gmail.com>
2020-08-06 11:47:22 +0200

    Better README files and minor changes in project 004

---
+++ src/004_DnCNN Numbers/2_train.py
@@ -0,0 +1,39 @@
+# github.com/Atamarado
+# In this program we get data generated with 1_generate_data.py and create a neural network
+# model and export it
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+IMG_SIZE = 28
+
+# Load datasets
+x_train = np.load('x_train.npy')
+y_train = np.load('y_train.npy')
+x_test = np.load('x_test.npy')
+y_test = np.load('y_test.npy')
+
+print(x_train.shape)
+
+# Model definition
+model = models.Sequential()
+model.add(layers.Conv2D(1, (3,3), activation='relu', input_shape=(IMG_SIZE, IMG_SIZE, 1), padding='same'))
+# model.add(layers.MaxPooling2D((2,2), padding='same'))
+model.add(layers.Conv2D(2, (3,3), activation='relu', padding='same'))
+model.add(layers.Conv2DTranspose(2, (3,3), activation='relu', padding='same'))
+#model.add(layers.UpSampling2D((2,2)))
+model.add(layers.Conv2DTranspose(1, (3,3), activation='relu', padding='same'))
+
+#model.summary()
+
+model.compile(optimizer=tf.keras.optimizers.Adagrad(learning_rate=0.1), loss=tf.keras.losses.MeanSquaredError())
+
+# Train model
+model.fit(x=x_train, y=y_train, epochs = 5,
+                    validation_data=(x_test, y_test))
+
+# Save model
+model.save('model.h5')
+
+
